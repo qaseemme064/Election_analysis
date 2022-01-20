4d6c5226eb48c89b5f54dcfde20931fff65a4121
qaseemme064 <qaseem_nedian@hotmail.com>
2022-01-19 23:46:23 -0500

    Added practice codes and PyPoll and written analysis file

---
+++ Python Practice/Python practice 10.py
@@ -0,0 +1,4 @@
+list_data = [{"alpha" : "a", 'value' : 1},{"alpha" : "b", 'value' : 2},{"alpha" : "c", 'value' : 3}]
+for name_list in list_data:
+    for value in name_list.values():
+       print(value)
---
+++ Python Practice/python practice 08.py
@@ -0,0 +1,11 @@
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+for county, value in counties_dict.items():
+    print(f"{county} county has {value:,} registered voters.")
+
+
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, {"county":"Denver", "registered_voters": 463353}, {"county":"Jefferson", "registered_voters": 432438}]
+for county_dict in voting_data:
+    for county, value in county_dict.items():
+        print(f"{county} county has {value} registered voters")
+        
